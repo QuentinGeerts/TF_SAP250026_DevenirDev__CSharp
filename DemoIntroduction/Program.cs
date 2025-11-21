0679a5633042798d72ef530f783a927c2f9324c9
Quentin Geerts <geerts.quentin@gmail.com>
2025-11-21 12:38:42 +0100

    Initial Commit

---
+++ DemoIntroduction/Program.cs
@@ -0,0 +1,42 @@
+﻿// Démonstration 01 - Introduction
+
+/*
+ * Commentaire
+ * Sur
+ * Plusieurs
+ * Lignes
+ */
+
+// 1. Écrire dans la console
+// Utilisation la bibliothèque System
+// Utilisation de la classe Console
+// Utilisation de la méthode "WriteLine" et "Write"
+
+// ".": Opérateur d'accès aux membres
+
+//Console.WriteLine('L\'ambulance arrive à toute allure.');
+Console.WriteLine("L'ambulance arrive à toute allure.");
+Console.WriteLine("Tu t'appelles \"Quentin\"");
+
+Console.Write("Hello");
+Console.Write("World !");
+
+
+// 2. Lecture depuis la console
+// Utilisation la bibliothèque System
+// Utilisation de la classe Console
+// Utilisation de la méthode "ReadLine"
+
+Console.WriteLine("Tu as tapé: " + Console.ReadLine());
+
+Console.WriteLine("Terminé.");
+
+
+// 3. Nettoyage de la console
+// Utilisation de la bibliothèque System
+// Utilisation de la classe Console
+// Utilisation de la méthode "Clear"
+
+Console.Clear();
+
+Console.WriteLine("La console est nettoyée...");
---
+++ DemoIntroductionWithoutTopLevel/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoIntroductionWithoutTopLevel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
