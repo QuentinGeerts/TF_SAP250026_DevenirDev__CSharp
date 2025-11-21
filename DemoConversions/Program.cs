e9f99ed07af842de151c871d7e92064639dfd3cf
Quentin Geerts <geerts.quentin@gmail.com>
2025-11-21 17:04:44 +0100

    Démonstration 03 - Conversions + Exercice 01 - InformationsUtilisateur

---
+++ DemoConversions/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Démonstration 03 - Conversions
+ */
+
+// 1.  ToString()
+// Convertir type → string
+
+int a = 42;
+
+Console.WriteLine(a.GetType());
+Console.WriteLine(a.ToString());
+
+// 2.  Classe Convert
+// Convertir type base → type base
+
+string b = "42";
+int bConverted = Convert.ToInt32(b);
+
+
+// 3.  Méthode Parse()
+// Convertir string → type base
+
+//string c = null; // ArgumentNullException
+//string c = "a"; // FormatException
+//string c = (4_000_000_000).ToString(); // OverflowException
+string c = "42";
+int cConverted = int.Parse(c);
+
+// 4.  Méthode TryParse()
+// Convertir string → type base
+
+//string d = (4_000_000_000).ToString();
+string d = "42";
+
+bool conversionReussie = int.TryParse(d, out int dConverted);
+
+Console.WriteLine(conversionReussie);
+Console.WriteLine($"Valeur à convertir: {d}, type origine: {d.GetType()}");
+Console.WriteLine($"Valeur de dConverted: {dConverted}, type origine: {dConverted.GetType()}");
+
+int eConverted;
+bool conversionReussie2 = int.TryParse(d, out eConverted);
+
+Console.WriteLine(conversionReussie2);
+Console.WriteLine($"Valeur à convertir: {d}, type origine: {d.GetType()}");
+Console.WriteLine($"Valeur de dConverted: {eConverted}, type origine: {eConverted.GetType()}");
+
---
+++ ExerciceInformationsUtilisateur/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Écrire un programme qui permet de demander à l'utilisateur ses informations 
+ * et ensuite les afficher.
+ * Un utilisateur est caractérisé par :
+ * - un nom (string)
+ * - un prénom (string)
+ * - une date de naissance (jour (byte), mois (byte) et année (short)) 
+ *  [rechercher sur internet comment créer une date] DateOnly
+ * - une adresse (rue (string), numéro (string), code postal (string) et ville (string))
+ * - s'il est marié ou non (bool)
+ * - une taille (en mètre) (float / double / Décimal)
+ */
+
+// Déclaration
+
+string nom, prenom, rue, numero, codePostal, ville;
+byte jour, mois;
+short annee;
+DateOnly dateNaissance;
+bool estMarie;
+float taille;
+
+// Initialisation
+
+Console.WriteLine("Entrez votre nom:");
+nom = Console.ReadLine();
+
+Console.WriteLine("Entrez votre prénom:");
+prenom = Console.ReadLine();
+
+
+Console.WriteLine("Entrez votre jour de naissance:");
+byte.TryParse(Console.ReadLine(), out jour);
+
+Console.WriteLine("Entrez votre mois de naissance:");
+byte.TryParse(Console.ReadLine(), out mois);
+
+Console.WriteLine("Entrez votre mois de naissance:");
+short.TryParse(Console.ReadLine(), out annee);
+
+dateNaissance = new DateOnly(annee, mois, jour);
+
+Console.WriteLine($"Êtes-vous marié•e ?");
+estMarie = Console.ReadLine() == "true";
+
+Console.WriteLine($"Quelle est ta taille ? (en mètre)");
+float.TryParse(Console.ReadLine(), out taille);
+
+
+Console.WriteLine($"{nom} {prenom}, né le {dateNaissance}");
