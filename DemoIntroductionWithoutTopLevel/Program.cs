e4ed92dd4615bb871a20e931d84965901151e047
Quentin Geerts <geerts.quentin@gmail.com>
2025-11-21 15:10:03 +0100

    Démonstration 02 - Variables

--- DemoIntroductionWithoutTopLevel/Program.cs
+++ DemoIntroductionWithoutTopLevel/Program.cs
@@ -2,9 +2,18 @@
 {
     internal class Program
     {
+        int age; // = default(int); 
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!" + args);
+        }
+
+        void Main2 ()
+        {
+            int age2;
+            Console.WriteLine(this.age);
+            //Console.WriteLine(age2);
         }
     }
 }
---
+++ DemoVariables/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Démonstration 02 - Variables
+ */
+
+// 1. Déclaration
+
+// Pseudo-Code: Variable nomVariable : Type
+// C#: Type nomVariable;
+
+string prenom;
+
+
+// 2. Affectation
+// 1ère affectation → Initialisation
+// Pseudo-Code: nomVariable ← valeur
+// C#: nomVariable = valeur;
+
+Console.WriteLine("Entrez votre prénom: ");
+prenom = Console.ReadLine();
+
+//Console.WriteLine("Tu t'appelles " + prenom);
+
+// Interpolation de chaînes de caractères
+Console.WriteLine($"Tu t'appelles {prenom}");
+
+
+// 3. Portée d'une variable
+
+if (true)
+{
+    Console.WriteLine(prenom);
+    string nom = "Geerts";
+    Console.WriteLine(nom);
+}
+
+// Console.WriteLine(nom); // En dehors du context du if
+
+
+// 4.  Constantes
+// Pseudo-Code: Constante NOM_CONSTANTE ← valeur
+// C#: const type NOM_CONSTANTE = valeur;
+
+const double PI = 3.141592;
+// PI = 3.1415; // Impossible de modifier une constante
+
+
+// 5.  Nullable
+// - Types valeurs: Ne sont pas IMPLICITEMENT nullables
+// - Types références: Sont IMPLICITEMENT nullables
+
+string variableImplicitementNullable = null;
+//int variableNonImplicitementNullable = null;
+//Nullable<int> variableNonImplicitementNullable = null;
+int? variableNonImplicitementNullable = null;
