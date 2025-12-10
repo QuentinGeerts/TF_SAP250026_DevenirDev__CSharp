177a8296f05f5afd6d6a5cc60c41409c047ef2e6
Quentin Geerts <geerts.quentin@gmail.com>
2025-12-10 16:32:47 +0100

    Correction exercice bonus bissextile

---
+++ ExercicesBonus01Bissextile/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Réaliser un petit algorithme qui sur base d’une année donnée va déterminer 
+ * s’il s’agit d’une année bissextile. Une année est bissextile si elle est 
+ * divisible par 4, mais non divisible par 100. Ou si elle est divisible par 400.
+ */
+
+Console.WriteLine($"Entrez une année : ");
+int annee = int.Parse( Console.ReadLine()! );
+
+bool estDivPar4 = annee % 4 == 0;
+bool estDivPar100 = annee % 100 == 0;
+bool estDivPar400 = annee % 400 == 0;
+
+if (estDivPar4 && !estDivPar100 || estDivPar400)
+{
+    Console.WriteLine($"{annee} est bissextile");
+}
+else
+{
+    Console.WriteLine($"{annee} n'est pas bissextile");
+}
---
+++ Methodes/Program.cs
@@ -0,0 +1,30 @@
+﻿
+bool estPair (int entier)
+{
+    return entier % 2 == 0;
+}
+bool estPremier(int nb)
+{
+    if (nb < 0) throw new Exception("Un nombre premier ne peut pas être négatif");
+    if (nb <= 2) return false;
+
+    for (int i = 2; i <= Math.Sqrt(nb); i++)
+    {
+        if (nb % i == 0) return false;
+    }
+
+    return true;
+}
+
+
+// -- Programme principal 
+
+int monEntier = 42;
+
+Console.WriteLine($"42 est pair ? " + estPair(monEntier));
+
+for (int i = 0; i < 100; i++)
+{
+    if (estPremier(i)) Console.WriteLine($"{i} est premier");
+}
+
