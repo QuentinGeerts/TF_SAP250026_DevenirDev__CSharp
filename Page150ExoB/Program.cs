2a6dc21e55d8d492ad0e4e79811273f1915e691d
Quentin Geerts <geerts.quentin@gmail.com>
2025-12-12 09:58:28 +0100

    Correction page 150 exercice B

---
+++ Page150ExoB/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ *  Calculer le factoriel d’un nombre entré au clavier
+ */
+
+Console.WriteLine($"Entrez un nombre: ");
+int.TryParse(Console.ReadLine(), out int nb);
+int res = 1;
+
+for (int i = 0; i < nb - 1; i++)
+{
+    //res = res * (nb - i);
+    res *= (nb - i);
+}
+
+Console.WriteLine($"!{nb} = {res}");
