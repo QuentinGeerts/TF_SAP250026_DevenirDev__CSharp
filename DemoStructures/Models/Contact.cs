bba0fc900ee8f43501405c8e3a4f3eca0e98a126
Quentin Geerts <geerts.quentin@gmail.com>
2025-12-16 15:39:09 +0100

    Démonstration 10 - Les structures

---
+++ DemoStructures/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿namespace DemoStructures.Models;
+
+public struct Contact
+{
+    public string LastName;
+    public string FirstName;
+    public string Email;
+    public string PhoneNumber;
+    public DateOnly BirthDate;
+
+    public string Informations()
+    {
+        return $"Contact: {LastName} {FirstName}";
+    }
+}
---
+++ DemoStructures/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Démonstration 10 - Structures
+ */
+
+using DemoStructures.Models;
+
+Contact c1 = new Contact();
+c1.FirstName = "Quentin";
+c1.LastName = "Geerts";
+c1.Email = "quentin.geerts@bstorm.be";
+c1.BirthDate = new DateOnly(1996, 4, 3);
+
+Console.WriteLine($"C1: {c1.Informations()}");
+
+Contact c2 = new Contact
+{
+    FirstName = "John",
+    LastName = "Doe"
+};
+
+void passageParStruct(Contact c)
+{
+    Console.WriteLine($"{c.FirstName}"); // Quentin
+    c.FirstName = "Technofutur";
+    Console.WriteLine($"{c.FirstName}"); // Technofutur
+}
+
+Console.WriteLine($"c1: {c1.FirstName}"); // Quentin
+passageParStruct(c1);
+Console.WriteLine($"c1: {c1.FirstName}"); // Quentin
